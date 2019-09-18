020508f68eeb2100c5f13d4a5e251c891cb1c1e3
Opilane TTHK <opilane@tthk.ee>
2019-09-18 12:55:26 +0300

    Add project files.

---
+++ ConsoleApp4/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public ShoppingCart ShoppingCart { get; set; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public override string ToString() //Kuna igal klassil on oma ToString, siis override kirjutab olemasoleva üle, et saaks kasutada enda ToStringi.
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Shop
+            List<Food> groceries = new List<Food> // lisame kohe listi toidud.
+            {
+                new Food("banana", 1.2), // kilohind, 1.2 eurot banaani eest oleks liiga kallis :D
+                new Food("potatoes", 1),
+                new Food("icecream", 2.5)
+            };
+
+            // Client
+            Console.WriteLine("Please enter your first name!");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Hi, " + firstName + "! Please enter your last name:");
+            string lastName = Console.ReadLine();
+
+            Person client = new Person(firstName, lastName);
+
+            Console.WriteLine("Welcome to our shop, " + client + "!");
+            client.ShoppingCart = new ShoppingCart();
+
+            ChooseFood(groceries, client);
+            while (Console.ReadLine() == "Y")
+            {
+             ChooseFood(groceries, client);
+            }
+            double sum = client.ShoppingCart.CalculateSum();
+            Console.WriteLine("Your total is " + sum + " Thank you for visiting!");
+
+           
+
+
+
+
+        }
+
+        private static void ChooseFood(List<Food> groceries, Person client)
+        {
+            Console.WriteLine("What do you want to buy?");
+            string foodName = Console.ReadLine();
+            Food chosenFood = groceries.FirstOrDefault(x => x.Name == foodName);
+            if (chosenFood == null)
+            {
+                Console.WriteLine("Excuse me, we don't have " + foodName + " in our shop.");
+            }
+            else
+            {
+                Console.WriteLine("How much?");
+                string amount = Console.ReadLine(); // amount on string, kuna ReadLine võtab ainult stringi vastu, hiljem tuleb teha Parse.
+                int a;
+                bool success = int.TryParse(amount, out a); //TryParse kontrollib, kas oli ikka arv või klient kirjutas midagi muud.
+                while (!success) // ! tähendab sama mis false.
+                {
+                    Console.WriteLine("Excuse me, amount should be integer value: ");
+                    amount = Console.ReadLine();
+                    success = int.TryParse(amount, out a);
+                }
+                client.ShoppingCart.AddToCart(chosenFood, a);
+            }
+            Console.WriteLine("Anything else? Y/N?");
+        }
+    }
+}
